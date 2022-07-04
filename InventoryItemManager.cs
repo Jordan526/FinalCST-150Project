40ddb56040901d0b9ee51b6de358a6b168fe8fa8
Jordan526 <96961388+Jordan526@users.noreply.github.com>
2022-07-04 02:03:30 -0700

    Add files via upload

---
+++ InventoryItemManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Milestone3
+{
+	class Item
+	{
+
+		//Global Variables for easy access
+		string itemName;
+		double itemPrice;
+		int itemQuantity;
+
+        //Constructors
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public Item(string name, double price, int quantity)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+			ItemName = name;
+			ItemPrice = price;
+			Quantity = quantity;
+		}
+
+		//Getter/Setters
+		public string ItemName { get => itemName; set => itemName = value; }
+		public double ItemPrice { get => itemPrice; set => itemPrice = value; }
+		public int Quantity { get => itemQuantity; set => itemQuantity = value; }
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using Milestone3;
+
+namespace Inventory_Management
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            InventoryManager inventoryManager = new InventoryManager();
+            int userChoice;
+
+            for (; ; )
+            {
+
+                Console.WriteLine("1---------|Add item to Inventory|");
+                Console.WriteLine("2---------|Display Inventory|");
+                Console.WriteLine("3---------|Remove item from Inventory|");
+                Console.WriteLine("4---------|Search item by name in Inventory|");
+                Console.WriteLine("5---------|Search item by price in Inventory|");
+                Console.WriteLine("6---------|Re-adjust item quantity in Inventory|");
+                Console.WriteLine("7---------|Exit Inventory Manager|");
+                Console.WriteLine("\n");
+
+
+                Console.WriteLine("Enter choice");
+                userChoice = Convert.ToInt32(Console.ReadLine());
+
+
+                switch (userChoice)
+                {
+                    case 1:
+                        inventoryManager.addItemToInventory();
+                        break;
+                    case 2:
+                        inventoryManager.displayInventoryItem();
+                        break;
+
+                    case 3:
+                        inventoryManager.removeItemFromInventoryList();
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter item name to search ");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+                        string nameOfItem = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+                        inventoryManager.searchInventoryItemByName(nameOfItem);
+                        break;
+
+                    case 5:
+
+                        Console.WriteLine("Enter price to search ");
+                        double priceToSearch = Convert.ToDouble(Console.ReadLine());
+                        inventoryManager.searchItemByPrice(priceToSearch);
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Enter item name to edit stock ");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+                        string editItemName = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+                        inventoryManager.modifyInventoryItem(editItemName);
+                        break;
+
+                    case 7:
+                        Console.WriteLine("Goodbye!");
+                        Environment.Exit(0);
+                        break;
+
+                }
+
+            }
+
+        }
+    }
+}
+
